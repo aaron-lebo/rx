41b4294e53aa3f1f65223a8152f1ebcec50895c0
aaron <you@example.com>
2019-01-21 23:31:55 -0600

    subreddit counts

--- rx.py
+++ rx.py
@@ -1,16 +1,30 @@
+import argparse
+import collections
 import json
 import lzma
 import sys
 
-total = int(sys.argv[2]) if len(sys.argv) > 2 else 0
-things = []
-with lzma.open(sys.argv[1]) as f:
-    try:
-        for n, line in enumerate(f):
-            things.append(json.loads(line))
-            if total and n + 1 == total:
-                break
-    except EOFError:
-        pass
+parser = argparse.ArgumentParser()
+parser.add_argument('command', choices=['count-subs', 'sample'])
+parser.add_argument('files', nargs='*')
+args = parser.parse_args()
 
-json.dump(things, sys.stdout, indent=2)
+total = 0#int(sys.argv[2]) if len(sys.argv) > 2 else 0
+count, things = collections.Counter(),  []
+for file in args.files:
+    with lzma.open(file) as f:
+        try:
+            for n, line in enumerate(f):
+                thing = json.loads(line)
+                count[thing['subreddit']] += 1
+                things.append(thing)
+                if total and n + 1 == total:
+                    break
+        except EOFError:
+            pass
+
+if args.command == 'count-subs':
+    for item in count.most_common():
+        print(item)
+else:
+    json.dump(things, sys.stdout, indent=2)
