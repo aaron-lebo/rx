83e996008e32d3ea354d0f60496eb880b5e19303
aaron <aaron.matthew.lebo@gmail.com>
2021-11-15 13:28:21 -0600

    ...

--- match.py
+++ match.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+import json
+
 import spacy
 from spacy.matcher import PhraseMatcher
 from spacy.tokens import DocBin
@@ -25,13 +28,16 @@ def main(tag: str, terms_file: str, files: List[str]):
         bin = DocBin().from_disk(f)
         bar = tqdm(bin.get_docs(nlp.vocab), total=len(bin))
         bar.set_description(f'{i}/{len(files)} {f}')
+        matches, combos_ = defaultdict(set), defaultdict(set)
         for d in bar:
             ms = matcher(d)
             if not ms: 
                 continue
 
             ms = {d[bg:nd].text.lower() for _, bg, nd in ms}
-            d.user_data[tag] = ms
+            ms_ = ','.join(ms)
+            id = d.user_data['id']
+            matches[ms_].update(id)
             for x in combos:
                 ok = True
                 for y in x:
@@ -40,10 +46,14 @@ def main(tag: str, terms_file: str, files: List[str]):
                         break
 
                 if ok:
-                    d.user_data[f'{tag}/'] = tuple(ms) 
+                    combos_[ms_].update(id)
                     break
 
-        bin.to_disk(f)
+        with open(f.replace('.spacy', '.json'), 'wt') as f:
+            json.dump(dict(
+                matches = {k: list(v) for k, v in matches.items()}, 
+                combos = {k: list(v) for k, v in combos_.items()}
+            ), f)
 
 if __name__ == '__main__':
     typer.run(main)
