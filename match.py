91db28431dbb6176ac4336a246b2615a5573052a
aaron <aaron.matthew.lebo@gmail.com>
2021-11-16 12:14:32 -0600

    ...

--- match.py
+++ match.py
@@ -1,6 +1,6 @@
-from collections import defaultdict
-import json
+import os
 
+import pandas as pd
 import spacy
 from spacy.matcher import PhraseMatcher
 from spacy.tokens import DocBin
@@ -11,7 +11,7 @@ from typing import List
 nlp = spacy.load('en_core_web_lg')
 
 def main(tag: str, terms_file: str, files: List[str]):
-    terms, combos = set(), []
+    terms, combos, raw = set(), [], []
     with open(terms_file) as f:
         for x in f:
             ands = []
@@ -20,25 +20,25 @@ def main(tag: str, terms_file: str, files: List[str]):
                 terms.update(ands[-1])
 
             combos.append(ands)
+            raw.append(x.strip())
 
     m = matcher = PhraseMatcher(nlp.vocab, attr='LOWER')
     m.add(tag, [nlp.make_doc(x) for x in terms])
 
+    os.makedirs('out/match', exist_ok=True)
     for i, f in enumerate(files):
         bin = DocBin().from_disk(f)
         bar = tqdm(bin.get_docs(nlp.vocab), total=len(bin))
         bar.set_description(f'{i}/{len(files)} {f}')
-        matches, combos_ = defaultdict(set), defaultdict(set)
+        results = [] 
         for d in bar:
             ms = matcher(d)
             if not ms: 
                 continue
 
             ms = {d[bg:nd].text.lower() for _, bg, nd in ms}
-            ms_ = ','.join(ms)
-            id = d.user_data['id']
-            matches[ms_].update(id)
-            for x in combos:
+            res = [d.user_data['id'], ','.join(ms), '']
+            for j, x in enumerate(combos):
                 ok = True
                 for y in x:
                     if not ms & y: 
@@ -46,14 +46,18 @@ def main(tag: str, terms_file: str, files: List[str]):
                         break
 
                 if ok:
-                    combos_[ms_].update(id)
+                    res[2] = raw[j]
                     break
 
-        with open(f.replace('.spacy', '.json'), 'wt') as f:
-            json.dump(dict(
-                matches = {k: list(v) for k, v in matches.items()}, 
-                combos = {k: list(v) for k, v in combos_.items()}
-            ), f)
+            results.append(res)
+
+    f = f'out/match/{os.path.split(f)[1].replace(".spacy", ".csv")}'
+    if os.path.isfile(f):
+        raise FileExistsError
+
+    df = pd.DataFrame(results, columns=['id', 'matches', 'combos'])
+    df = df.set_index('id')
+    df.to_csv(f)
 
 if __name__ == '__main__':
     typer.run(main)
