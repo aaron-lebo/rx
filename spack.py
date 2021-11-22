1f17386f10bc8e2cb8bf29e9d2351b12fae01078
aaron <aaron.matthew.lebo@gmail.com>
2021-11-22 13:15:00 -0600

    ...

--- spack.py
+++ spack.py
@@ -16,34 +16,38 @@ def load(s: str):
     x = orjson.loads(s)
     return '\n'.join([x['title'], x['selftext']]), x
 
+def save(bin: DocBin, pre: str, utc: datetime.date):
+    f = f'{pre}{utc}.spacy'
+    bin1 = DocBin(store_user_data=True).from_disk(f)
+    bin1.merge(bin)
+    bin1.to_disk(f)
+
 def main(file: str):
-    pre = file.split('/')[-1][:3].lower()
+    file1 = file.split('/')[-1].lower()
+    pre = file1[:3]
     assert(pre in ('rc_', 'rs_'))
+    with open(file) as f:
+        n = sum(1 for x in f) 
+        f.seek(0)
 
-    f = open(file) 
-    n = sum(1 for x in f) 
-    f.seek(0)
+        n_proc = os.cpu_count()
+        n_proc = 1 if n_proc == 1 else n_proc-1
+        p = nlp.pipe((load(x) for x in f), as_tuples=True, n_process=n_proc)
+        bin = DocBin(store_user_data=True)
+        utc = None
+        for i, (d, x) in enumerate(tqdm(p, total=n)):
+            d.user_data = {k: x.get(k) for k in ks}
+            for k in ks_int:
+                d.user_data[k] = int(x.get(k, 0))
 
-    xs = (load(x) for x in f)
-    n_proc = os.cpu_count()
-    p = nlp.pipe(xs, as_tuples=1, n_process=1 if n_proc == 1 else n_proc-1)
-    bin = DocBin(store_user_data=True)
-    utc = None
-    for i, (d, x) in enumerate(tqdm(p, total=n)):
-        d.user_data = {k: x.get(k) for k in ks}
-        for k in ks_int:
-            d.user_data[k] = int(x.get(k, 0))
+            bin.add(d)
 
-        bin.add(d)
+            utc1 = utc
+            utc = datetime.fromtimestamp(d.user_data['created_utc']).date()
+            if utc1 and utc != utc1:
+                save(bin, pre, utc)
 
-        utc1 = utc
-        utc = datetime.fromtimestamp(d.user_data['created_utc']).date()
-        if utc1 and utc != utc1:
-            bin.to_disk(f'{pre}{utc}.spacy')
-            bin = DocBin(store_user_data=True)
-
-    bin.to_disk(f'{pre}{utc}.spacy')
-    f.close()
+        save(bin, pre, utc)
 
 if __name__ == '__main__':
     typer.run(main)
