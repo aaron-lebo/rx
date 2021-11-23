018df4c016ed6c7b8f3219779517de8a038ba6c6
aaron <aaron.matthew.lebo@gmail.com>
2021-11-23 13:56:57 -0600

    .

--- count.py
+++ count.py
@@ -11,14 +11,13 @@ from typing import List
 nlp = spacy.load('en_core_web_lg')
 
 def main(files: List[str]):
-    results =  []
+    n = 0 
     for i, f in enumerate(files):
         bin = DocBin().from_disk(f)
-        print(i, f, len(bin))
+        print(f'{i:03} {f} {len(bin)}')
+        n += len(bin)
 
-    df = pd.DataFrame(results, columns=['id', 'matches', 'combos'])
-    df = df.set_index('id')
-    df.to_csv(f)
+    print(i, n) 
 
 if __name__ == '__main__':
     typer.run(main)
