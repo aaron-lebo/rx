ffff0ae55bec2632d442d876ce9d05a05460c5b4
aaron <aaron.matthew.lebo@gmail.com>
2021-11-22 19:24:05 -0600

    .

--- spack.py
+++ spack.py
@@ -27,20 +27,21 @@ def main(file: str):
         n_proc = os.cpu_count()
         n_proc = 1 if n_proc == 1 else n_proc-1
         p = nlp.pipe((load(x) for x in f), as_tuples=True, n_process=n_proc)
-        bin = DocBin(store_user_data=True)
         utc = None
+        bin = DocBin(store_user_data=True)
         for i, (d, x) in enumerate(tqdm(p, total=n)):
             d.user_data = {k: x.get(k) for k in ks}
             for k in ks_int:
                 d.user_data[k] = int(x.get(k, 0))
-
-            bin.add(d)
 
             utc1 = utc
             utc = datetime.fromtimestamp(d.user_data['created_utc']).date()
             if utc1 and utc != utc1:
                 f = f'{pre}{utc1}.spacy'
                 bin.to_disk(f)
+                bin = DocBin(store_user_data=True)
+
+            bin.add(d)
 
         bin.to_disk(f)
 
