1e96eee5d004dd27f1bef7d045ecf0527dbdd899
aaron <aaron.matthew.lebo@gmail.com>
2021-11-23 21:50:33 -0600

    .

--- print.py
+++ print.py
@@ -5,10 +5,11 @@ import typer
 nlp = spacy.load('en_core_web_lg')
 
 def main(file: str, i: int):
-    bin = DocBin().from_disk(f)
+    bin = DocBin().from_disk(file)
     for j, d in enumerate(bin.get_docs(nlp.vocab)):
         if i == j:
             print(d.user_data)
+            break
 
 if __name__ == '__main__':
     typer.run(main)
