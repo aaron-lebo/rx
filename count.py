1130a2572b3e997d4c1d3225d7536297a823f585
aaron <aaron.matthew.lebo@gmail.com>
2021-12-31 17:21:35 -0600

    update

--- count.py
+++ count.py
@@ -1,18 +1,34 @@
 import spacy
 from spacy.tokens import DocBin
+from tqdm import tqdm
 import typer
-from typing import List
+from typing import List as lst
 
+app = typer.Typer()
 nlp = spacy.load('en_core_web_lg')
 
-def main(files: List[str]):
-    n = 0 
-    for i, f in enumerate(files):
-        bin = DocBin().from_disk(f)
-        print(f'{i:03} {f} {len(bin)}')
-        n += len(bin)
+with open('stats.txt') as f:
+    fs = (x.split() for x in f)
+    fs = {x[0].split('.')[0].lower(): int(x[1]) for x in fs}
 
-    print(f'{i+1:03} {n}')
+@app.command()
+def check(files: lst[str]):
+    ids = set()
+    for f in tqdm(files, total=len(files)):
+        bin = DocBin().from_disk(f)
+        ids.update({x.user_data['id'] for x in bin.get_docs(nlp.vocab)})
+
+    print(len(ids))
+    assert(fs[f.split('/')[1].split('.')[0]] == len(ids))
+
+@app.command()
+def run(files: lst[str]):
+    n = 0 
+    for f in tqdm(files, total=len(files)):
+        bin = DocBin().from_disk(f)
+        n += len(bin) 
+
+    print(n)
 
 if __name__ == '__main__':
-    typer.run(main)
+    app()
