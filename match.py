7419be79978633bb85357b9a3a9e90f0bb59c1b4
aaron <aaron.matthew.lebo@gmail.com>
2022-01-08 09:37:20 -0600

    check in

--- match.py
+++ match.py
@@ -1,16 +1,73 @@
+import glob
 import os
 
 import pandas as pd
 import spacy
-from spacy.matcher import PhraseMatcher
+from spacy import matcher
 from spacy.tokens import DocBin
 from tqdm.auto import tqdm
 import typer
-from typing import List
 
+app = typer.Typer()
 nlp = spacy.load('en_core_web_lg')
 
-def main(tag: str, terms_file: str, files: List[str]):
+def data(dat, cols):
+    df = pd.DataFrame(dat, columns=cols.split())
+    df = df.set_index('id')
+    return df
+
+def clean(txt: str):
+    prs = {'(': ')', '[': ']', '{': '}'}
+    stk = []
+    for i, x in enumerate(txt):
+        if x in prs:
+            stk.append((i, x))
+        elif stk and x == prs[stk[-1][1]]:
+            stk.pop()
+        elif stk and x in prs or x in prs.values():
+            txt = txt[:i]
+    return txt[:stk[0][0]] if stk else txt
+
+def save(df, file):
+    f = f'out/match/{os.path.split(file)[1].replace(".spacy", ".csv")}'
+    if os.path.isfile(f):
+        raise FileExistsError
+    df.to_csv(f)
+
+@app.command()
+def urls(dir: str):
+    fs = glob.glob(f'{dir}/*.spacy')
+    m = match = matcher.Matcher(nlp.vocab)
+    m.add('url',  [[{'LIKE_URL': True}], [{'LOWER': {'REGEX': r'\)\[http(.*)'}}]])
+    os.makedirs('out/match', exist_ok=True)
+    for i, f in enumerate(fs):
+        bin = DocBin().from_disk(f)
+        bar = tqdm(bin.get_docs(nlp.vocab), total=len(bin))
+        bar.set_description(f'{i}/{len(fs)} {f}')
+        dat = [] 
+        for d in bar:
+            for _, y, z in match(d):
+                txt = d[y:].text.split()[0]
+                i = txt.find('http')
+                i = i+4 if i > -1 else 0
+                j = txt.find('\n', i)
+                pre, txt = '', txt[i:j if j > -1 else len(txt)]
+                if txt.startswith('://'):
+                    pre, txt = 'p', txt[3:]
+                elif txt.startswith('s://'):
+                    pre, txt = 's', txt[4:]
+
+                txt = txt.replace('\\', '').strip()
+                dat.append([d.user_data['id'], pre, txt])
+
+        df = data(dat, 'id http url')
+        df.url = df.url.apply(clean)
+        df = df[~df.url.isna()]
+        save(df, f)
+
+@app.command()
+def main(tag: str, terms_file: str, files: str):
+    files = glob.glob(files)
     terms, combos, raw = set(), [], []
     with open(terms_file) as f:
         for x in f:
@@ -22,7 +79,7 @@ def main(tag: str, terms_file: str, files: List[str]):
             combos.append(ands)
             raw.append(x.strip())
 
-    m = matcher = PhraseMatcher(nlp.vocab, attr='LOWER')
+    m = matcher = matcher.PhraseMatcher(nlp.vocab, attr='LOWER')
     m.add(tag, [nlp.make_doc(x) for x in terms])
 
     os.makedirs('out/match', exist_ok=True)
@@ -51,13 +108,7 @@ def main(tag: str, terms_file: str, files: List[str]):
 
             results.append(res)
 
-    f = f'out/match/{os.path.split(f)[1].replace(".spacy", ".csv")}'
-    if os.path.isfile(f):
-        raise FileExistsError
-
-    df = pd.DataFrame(results, columns=['id', 'matches', 'combos'])
-    df = df.set_index('id')
-    df.to_csv(f)
+    save(f, results, 'id matches combos')
 
 if __name__ == '__main__':
-    typer.run(main)
+    app()
