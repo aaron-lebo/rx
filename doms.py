92f673da617a5ccff5c22433cb4590dbdcd86b1e
aaron <aaron.matthew.lebo@gmail.com>
2022-02-28 19:08:57 -0600

    x

--- doms.py
+++ doms.py
@@ -21,15 +21,15 @@ def get_wiki(x):
     return x.split('?title=')[-1]
 
 @app.command()
-def main(dir: str):
+def csvs(dir: str):
     dirs = sorted(f.path for f in os.scandir(dir) if f.is_dir())
     dirs1 = [] 
     for dir in dirs:
         f = split(dir)
-        if f[:3] in ('rc_', 'rs_') and not os.path.exists(f'out/dom/{f}.csv'):
+        if f[:3] in ('rc_', 'rs_') and not os.path.exists(f'out/doms/{f}.csv'):
             dirs1.append(dir) 
 
-    os.makedirs(f'out/dom', exist_ok=True)
+    os.makedirs(f'out/doms', exist_ok=True)
     for i, dir in enumerate(dirs1):
         f = split(dir)
         x = sorted(glob.glob(f'{dir}/*.csv'))
@@ -51,7 +51,32 @@ def main(dir: str):
         wiki = df.dom.str.match('^wikipedia\.(com|org)$')
         df.loc[wiki, 'wiki'] = df[wiki].url.map(get_wiki)
 
-        df['dom url id author subreddit wiki'.split()].to_csv(f'out/dom/{f}.csv', index=False)
+        df['dom url id author subreddit wiki'.split()].to_csv(f'out/doms/{f}.csv', index=False)
+
+subs = 'politics Conservative hillaryclinton SandersForPresident The_Donald'.split()
+
+@app.command()
+def csv(dir: str='out/doms'):
+    ds, t = [], tqdm(sorted(glob.glob(f'{dir}/*.csv')))
+    for f in t:
+        t.set_description(f)
+        df = pd.read_csv(f, usecols=['dom', 'author', 'subreddit'])
+        df = df[df.subreddit.isin(subs)]
+        for c in df.columns:
+            df[c] = df[c].astype('category')
+
+        ds.append(df)
+
+    df = pd.concat(ds).rename(columns=dict(author='usr', subreddit='sub'))
+    del ds
+
+    dc = df.dom.value_counts().to_frame('cites')
+    dc['%'] = dc.cites / dc.cites.sum() * 100
+    du = df.groupby(['dom']).usr.nunique().to_frame('users').sort_values('users', ascending=0)
+    dz = dc.join(du)
+    dz['cites/users'] = dc.cites / du.users
+    dz.to_csv('out/doms.csv')
+    print(dz)
 
 if __name__ == '__main__':
     app()
