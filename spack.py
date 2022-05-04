f10a22a7312ff0a40633dc2c3579cd87e17f10c2
aaron <aaron.matthew.lebo@gmail.com>
2022-05-04 16:42:14 -0500

    pack

--- spack.py
+++ spack.py
@@ -1,93 +1,75 @@
-import glob
+from datetime import datetime
 import json 
+import subprocess
 import os
 
 import pandas as pd
-import spacy
-from spacy.tokens import DocBin
 from tqdm.auto import tqdm
 import typer
 
 app = typer.Typer()
-Op = typer.Option
-nlp = spacy.load('en_core_web_lg')
 
 stats = pd.read_csv('stats.csv')
-stats.file = stats.file.str.lower()
-stats = dict(stats.values)
+ext = stats.file.str.split('.').str
+stats.file, stats['ext'] = ext[0].str.lower(), ext[1]
+
+ks = 'id created_utc edited retrieved_on author subreddit'.split()
+ks_com = 'link_id parent_id body'.split()
+ks_sub = 'url title selftext'.split()
+
+def save(dat, ks, file, day):
+    df = pd.DataFrame(dat, columns=ks).set_index('id')
+    df.created_utc = pd.to_datetime(df.created_utc, unit='s')
+    df.loc[df.edited==False, 'edited'] = None
+    df.edited = pd.to_datetime(df.edited, unit='s')
+    df.retrieved_on = pd.to_datetime(df.retrieved_on, unit='s')
+
+    pq = f'out/{file}-{day:02}.pq'
+    if os.path.exists(pq):
+        df = pd.concat([pd.read_parquet(pq), df])
+
+    df.to_parquet(pq)
 
 @app.command()
-def count(dir: str, check=True):
-    fs = glob.glob(f'{dir}/*.spacy')
-    n, ids = 0, set()
-    for f in tqdm(fs, total=len(fs)):
-        bin = DocBin().from_disk(f)
-        n += len(bin) 
-        if check:
-            df = pd.read_csv(f.replace('.spacy', '.csv'))
-            ids.update(df.id)
+def pack(file: str):
+    file1 = file.lower()
+    pre = file1[:3]
+    assert(pre in ('rc_', 'rs_'))
 
-    if check:
-        file = os.path.normpath(dir).split('/')[-1]
-        print(n, len(ids), stats[file])
-        assert(n == len(ids) == stats[file])
-    return n
-
-ks_ = 'id created_utc edited retrieved_on author subreddit'.split()
-ks_com = 'link_id parent_id'.split()
-ks_sub = ['url']
-
-def load(s: str, pre: str):
-    x = json.loads(s)
     if pre == 'rc_':
-        return x['body'], x
-    return '\n'.join([x['title'], x['selftext']]), x
+        subprocess.run(['wget', f'https://files.pushshift.io/reddit/comments/{file}.{stat.ext}'])
+        ks1 = ks+ks_com
+    else:
+        subprocess.run(['wget', f'https://files.pushshift.io/reddit/submissions/{file}.zst'])
+        ks1 = ks+ks_sub
 
-@app.command()
-def main(file: str, resume: bool = Op(False), start: int = Op(0), full: bool = Op(True)):
+    stat = stats[stats.file==file1].iloc[0]
+    if stat.ext == 'bz2':
+        subprocess.run(['bzip2', '-dv', file+'.bz2'])
+    else:
+        subprocess.run(['zstd', '-d', f'{file}.{stat.ext}', '--long=31'])
+
     with open(file) as f:
-        file = os.path.normpath(file).split('/')[-1].lower()
-        pre = file[:3]
-        ks = ks_+(ks_com if pre == 'rc_' else ks_sub)
-        assert(pre in ('rc_', 'rs_'))
-
         n = sum(1 for x in f)
-        assert(n == stats[file])
+        assert(n == stat.num)
         f.seek(0)
-        os.makedirs(f'out/{file}', exist_ok=True)
-        if resume and not start:
-            start = count(f'out/{file}', check=False)
-        if start:
-            n -= start
-            for i, _ in enumerate(f):
-                if i == start-1:
-                    break
-
-        p = (load(x, pre) for x in f)
-        p = nlp.pipe(p, as_tuples=True) if full else p
-        dat, bin = [], DocBin(store_user_data=True)
-        for i, (d, x) in enumerate(tqdm(p, total=n)):
-            dat.append([x.get(k) for k in ks])
-            if full:
-                d.user_data['id'] = x['id']
-                bin.add(d)
-
-            if i and not i % 20000 or n == i+1:
-                f = f'out/{file}/{x["id"]}.'    
-                if os.path.isfile(f+'csv'):
-                    raise FileExistsError
-                if full:
-                    if os.path.isfile(f+'spacy'):
-                        raise FileExistsError
-
-                    bin.to_disk(f+'spacy')
-                    bin = DocBin(store_user_data=True)
-
-                df = pd.DataFrame(dat, columns=ks).set_index('id')
-                df.to_csv(f+'csv')
+        os.makedirs('out/', exist_ok=True)
+        
+        dat, day = [], None
+        for i, x in tqdm(enumerate(f), total=n):
+            x = json.loads(x)
+            dt = datetime.utcfromtimestamp(int(x['created_utc']))
+            if day and day != dt.day:
+                save(dat, ks1, file1, day)
                 dat = []
-                
+
+            dat.append([x.get(k) for k in ks1])
+            day = dt.day
+
+        save(dat, ks1, file1, day)
         assert(n == i+1)
+
+    os.remove(file)
 
 if __name__ == '__main__':
     app()
